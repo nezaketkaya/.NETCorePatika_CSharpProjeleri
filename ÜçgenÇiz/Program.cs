5a0399d826f5ed1cdcfbef39dba99ce3d996e264
Nezaket Kaya <94640744+nezaketkaya@users.noreply.github.com>
2023-09-20 02:00:38 +0300

    Add files via upload

---
+++ ÜçgenÇiz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UcgenCizmeUygulamasi
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Üçgenin boyutunu girin:");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+
+            UcgenCiz.CizUcgen(boyut);
+        }
+    }
+
+    public static class UcgenCiz
+    {
+        public static void CizUcgen(int boyut)
+        {
+            for (int i = 1; i <= boyut; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
