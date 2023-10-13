22dfeb7d5c27ff26f85f5a1192539643a52443ef
Nezaket Kaya <94640744+nezaketkaya@users.noreply.github.com>
2023-10-13 15:20:44 +0300

    Add files via upload

---
+++ AlanHesaplama/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlanHesaplama
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+    }
+
+     class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+        public double Side3 { get; set; }
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public override double CalculateArea()
+        {
+            double s = (Side1 + Side2 + Side3) / 2;
+            return Math.Sqrt(s * (s - Side1) * (s - Side2) * (s - Side3));
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Side1 + Side2 + Side3;
+        }
+    }
+
+}
---
+++ AlanHesaplama/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace AlanHesaplama
+{
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Geometrik şekli seçin (Daire, Üçgen, Kare, Dikdörtgen, vb.):");
+        string shape = Console.ReadLine().ToLower();
+
+        double result = 0;
+
+        switch (shape)
+        {
+            case "daire":
+                Console.WriteLine("Dairenin yarıçapını girin: ");
+                double radius = Convert.ToDouble(Console.ReadLine());
+                Circle circle = new Circle(radius);
+
+                Console.WriteLine("Hesaplamak istediğiniz boyutu seçin (Çevre, Alan):");
+                string dimension = Console.ReadLine().ToLower();
+
+                if (dimension == "çevre")
+                {
+                    result = circle.CalculatePerimeter();
+                }
+                else if (dimension == "alan")
+                {
+                    result = circle.CalculateArea();
+                }
+                break;
+
+            case "üçgen":
+                Console.WriteLine("Üçgenin ilk kenarını girin: ");
+                double side1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Üçgenin ikinci kenarını girin: ");
+                double side2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Üçgenin üçüncü kenarını girin: ");
+                double side3 = Convert.ToDouble(Console.ReadLine());
+                Triangle triangle = new Triangle(side1, side2, side3);
+
+                Console.WriteLine("Hesaplamak istediğiniz boyutu seçin (Çevre, Alan):");
+                dimension = Console.ReadLine().ToLower();
+
+                if (dimension == "çevre")
+                {
+                    result = triangle.CalculatePerimeter();
+                }
+                else if (dimension == "alan")
+                {
+                    result = triangle.CalculateArea();
+                }
+                break;
+
+            // Diğer geometrik şekiller için benzer şekilde devam edebilirsiniz.
+
+            default:
+                Console.WriteLine("Geçersiz bir geometrik şekil girdiniz.");
+                break;
+
+                    
+            }
+           
+             if (result != 0)
+             {
+                 Console.WriteLine($"Sonuç: {result}");
+             }
+    }
+}
+}
