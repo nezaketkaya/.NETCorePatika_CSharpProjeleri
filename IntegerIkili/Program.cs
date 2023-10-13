ae0bb9cd920b3b4c2a323a008bdc56407b4075fd
Nezaket Kaya <94640744+nezaketkaya@users.noreply.github.com>
2023-10-13 15:35:02 +0300

    Add files via upload

---
+++ IntegerIkili/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Integer ikilileri boşlukla ayırarak girin:");
+        string input = Console.ReadLine();
+        string[] numbers = input.Split(' ');
+
+        if (numbers.Length % 2 != 0)
+        {
+            Console.WriteLine("Eksik veya tek sayıda integer girdiniz.");
+            return;
+        }
+
+        List<int> results = new List<int>();
+
+        for (int i = 0; i < numbers.Length; i += 2)
+        {
+            int num1 = int.Parse(numbers[i]);
+            int num2 = int.Parse(numbers[i + 1]);
+
+            if (num1 == num2)
+            {
+                results.Add((num1 + num2) * (num1 + num2));
+
+            }
+            else
+            {
+                results.Add(num1 + num2);
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", results));
+    }
+}
